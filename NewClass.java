2ed85fcc57520fa374651b283539f264d43e6632
FnisRC1801 <143724502+FnisRC1801@users.noreply.github.com>
2023-09-05 09:47:01 -0500

    La clase de las 5 tareas

---
+++ NewClass.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        
+        String tarea1 ="Lavar trastes";
+        String tarea2 = "limpear caja de arena";
+        String tarea3 = "Investigacion";
+        String tarea4 = "Cocinar";
+        String tarea5 ="Lavar ropa";
+        
+         System.out.println("Llegando a mi casa debo de " + tarea1+" y despues " + tarea2+ " de mi gata");
+        System.out.println("Mi tarea de hoy es una " + tarea3);
+        System.out.println("Antes de que mi hermano llegue debo "+tarea4+" para que coma");
+        System.out.println("Y al final debo de "+tarea5);
+        
+    }
+}
